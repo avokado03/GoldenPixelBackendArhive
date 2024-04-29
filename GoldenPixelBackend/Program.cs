79a6fd826e17ac26d8193a73271024fbc10b2157
Балашова Ангелина <abalashova@ptsecurity.com>
2024-04-29 12:56:58 +0300

    GP-41 Swagger

--- GoldenPixelBackend/Program.cs
+++ GoldenPixelBackend/Program.cs
@@ -1,4 +1,5 @@
 using GoldenPixel.Db;
+using IOS.ROST.WebApi.Common.Middleware.Configurations;
 using LinqToDB;
 using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
@@ -26,15 +27,16 @@ builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => SwaggerMiddlewareConfigurations.ConfigureSwaggerGenOptions(c));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwagger(c => 
+		c.RouteTemplate = "api/swagger/{documentName}/swagger.json");
+	app.UseSwaggerUI(c => SwaggerMiddlewareConfigurations.GetSwaggerUIOptions(c));
 }
 
 app.UseHttpsRedirection();
