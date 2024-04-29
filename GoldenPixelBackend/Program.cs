9c71a1b3f45adb5960775a499e77ec8be3abebea
Балашова Ангелина <abalashova@ptsecurity.com>
2024-04-29 12:46:21 +0300

    GP-45 add US in builder

--- GoldenPixelBackend/Program.cs
+++ GoldenPixelBackend/Program.cs
@@ -3,7 +3,10 @@ using LinqToDB;
 using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
 using LinqToDB.Common;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Configuration;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +17,13 @@ builder.Services.AddLinqToDBContext<GpDbConnection>((provider, options)
 				.UsePostgreSQL(builder.Configuration.GetConnectionString("GpDb"))
 				.UseDefaultLogging(provider));
 
+var environment = builder.Environment.EnvironmentName;
 builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("appsettings.json", false, true);
+builder.Configuration.AddJsonFile($"appsettings.{environment}.json", true);
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
