28741171a3f9e62c81dcecf99f5993e0a06d5e1f
Балашова Ангелина <abalashova@ptsecurity.com>
2024-05-05 15:08:46 +0300

    GP-46 add nlog

--- GoldenPixelBackend/Program.cs
+++ GoldenPixelBackend/Program.cs
@@ -1,48 +1,66 @@
+using GoldenPixel.Common.Middleware.Configurations;
 using GoldenPixel.Db;
-using IOS.ROST.WebApi.Common.Middleware.Configurations;
 using LinqToDB;
 using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
-using LinqToDB.Common;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Configuration;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
 using System.Reflection;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-builder.Services.AddLinqToDBContext<GpDbConnection>((provider, options)
-			=> options
-				.UsePostgreSQL(builder.Configuration.GetConnectionString("GpDb"))
-				.UseDefaultLogging(provider));
-
-var environment = builder.Environment.EnvironmentName;
-builder.Services.AddControllers();
-builder.Configuration.AddJsonFile("appsettings.json", false, true);
-builder.Configuration.AddJsonFile($"appsettings.{environment}.json", true);
-builder.Configuration.AddEnvironmentVariables();
-builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => SwaggerMiddlewareConfigurations.ConfigureSwaggerGenOptions(c));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-	app.UseSwagger(c => 
-		c.RouteTemplate = "api/swagger/{documentName}/swagger.json");
-	app.UseSwaggerUI(c => SwaggerMiddlewareConfigurations.GetSwaggerUIOptions(c));
+	builder.Services.AddLinqToDBContext<GpDbConnection>((provider, options)
+				=> options
+					.UsePostgreSQL(builder.Configuration.GetConnectionString("GpDb"))
+					.UseDefaultLogging(provider));
+
+	var environment = builder.Environment.EnvironmentName;
+	builder.Services.AddControllers();
+	builder.Configuration.AddJsonFile("appsettings.json", false, true);
+	builder.Configuration.AddJsonFile($"appsettings.{environment}.json", true);
+	builder.Configuration.AddEnvironmentVariables();
+	builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
+
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen(c => SwaggerMiddlewareConfigurations.ConfigureSwaggerGenOptions(c));
+
+	builder.Services.AddLogging(logger =>
+	{
+		logger.ClearProviders();
+		logger.SetMinimumLevel(LogLevel.Information);
+	});
+
+	builder.Services.AddSingleton<ILoggerProvider, NLogLoggerProvider>();
+
+	var app = builder.Build();
+
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger(c =>
+			c.RouteTemplate = "api/swagger/{documentName}/swagger.json");
+		app.UseSwaggerUI(c => SwaggerMiddlewareConfigurations.GetSwaggerUIOptions(c));
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch(Exception ex)
+{
+	logger.Error(ex);
+}
+finally
+{
+	LogManager.Shutdown();
+}
